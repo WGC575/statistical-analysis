1e4a123e40b50a010b6fb647c5e86453d67e517e
WGC575 <wgc575@gmail.com>
2019-02-28 14:43:14 -0800

    Success in running the Games-Howell Test.

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Codes for statistical anlaysis
 
 significance.py provide a method to deal with statistical significance of a set of data using Games-Howell
 
+#installation of rpy2
+1. Install R, set your R_HOME (...\R\R-3.0.2\) and PATH (...\R\R-3.0.2\bin\x64)
+2. Find the correct version of rpy2 you need in https://www.lfd.uci.edu/~gohlke/pythonlibs/
+3. Move the file to the Python directory and install rpy2 with "pip install rpy2-......whl"
+4. Set the R_USER (...\Python\Python36\site-packages\rpy2)
+5. Restart your machine to get the configuration finalized
+
+#Games-Howell Test
+For usage, see https://pypi.org/project/squaad/
--- significance.py
+++ significance.py
@@ -3,36 +3,13 @@ from squaad import file
 from squaad import db
 
 
-myConnection=db("config.json","cache")
+#myConnection=db("config.json","cache")
 
 stats=stats()
 
-affiliation=myConnection.getAffiliationCompilation()
+data = {"GROUP1":{True:100, False:3999}, "GROUP2":{True:2999,False:2939}}
 results={}
 
-groups={}
-for entry in affiliation:
-    affKey=entry['organization']+"_"+entry['status']
-    groups[affKey]={}
-    groups[affKey][True]=int(entry['c_impactful'])
-    groups[affKey][False]=int(entry['c_impactful'])-int(entry['c_analyzed'])
+results["comp"]=stats.gamesHowellBinomial(data)
 
-
-results["comp"]=stats.gamesHowellBinomial(groups)
-
-affiliation=myConnection.getAffiliationQuality()
-
-metrics= {'locs', 'smls', 'cpxs', 'vuls', 'fbgs', 'locs_inc', 'smls_inc', 'cpxs_inc', 'vuls_inc', 'fbgs_inc', 'locs_dec', 'smls_dec', 'cpxs_dec', 'vuls_dec', 'fbgs_dec'}
-
-#CONVERT RESULTS TO ARRAY FOR PASSING TO STATS CHECK
-for metric in metrics:
-
-    groups={}
-    for entry in affiliation:
-        groups[entry["organization"]+"_"+entry["status"]]={}
-        groups[entry["organization"]+"_"+entry["status"]][True]=int(entry[metric])
-        groups[entry["organization"]+"_"+entry["status"]][False]=int(entry["total"])-int(entry[metric])
-
-    results[metric]=stats.gamesHowellBinomial(groups)
-
-file.saveResultsExcel(results,"stat.xls")
+#file.saveResultsExcel(results,"stat.xls")
