40d19cff1769bd06f96517559fa35a50080d4d9a
WGC575 <wgc575@gmail.com>
2019-03-05 23:38:05 -0800

    Correct input for the G.H. Test.

---
+++ consistency.py
@@ -0,0 +1,37 @@
+from squaad import stats
+from squaad import file
+from readfcol import readData
+
+#Getting data from datafile (column-designed)
+col_true = {}
+col_false = {}
+
+#reading one column file
+datafile = open("add_ntl.data", "rt")
+col_true = readData(datafile)
+datafile = open("add_brk.data", "rt")
+col_false = readData(datafile)
+
+i = 0
+data_formatted = {}
+size = min(len(col_false), len(col_true))
+
+while i < size:
+    data_formatted["Group_" + str(i)] = {}
+    data_formatted["Group_" + str(i)][True] = int(col_true[i])
+    data_formatted["Group_" + str(i)][False] = int(col_false[i])
+    i += 1
+
+#Initiation
+stats=stats()
+results={}
+
+#debug
+print(data_formatted)
+
+#Result assignment
+results["output"]=stats.gamesHowellBinomial(data_formatted)
+
+#File output
+file.saveResultsExcel(results,"output.xls")
+pass
